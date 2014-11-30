03e6cfb2fbe0653555b193ccacbce53740316a6f
Deena Wang <thedeenawang@gmail.com>
2014-11-30 17:54:37 -0500

    Refactors api route into own file

--- habit-vps-app/app.js
+++ habit-vps-app/app.js
@@ -14,6 +14,7 @@ require('./models/Todos');
 // Must import routes after db so that models are defined
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ habit-vps-app/routes/api.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var Todo = mongoose.model('Todo');
+
+// Middleware function that tries to attach a todo object to requests with a "todo" parameter
+router.param('todoId', function(req, res, next, id) {
+  var query = Todo.findById(id);
+
+  query.exec(function(err, todo) {
+    if (err) { return next(err); }
+    if (!todo) {
+      return next(new Error("can't find todo"));
+    }
+    req.todo = todo;
+    return next();
+  });
+});
+
+// For all routes that don't need an ID
+router.route('/todos')
+.get(function(req, res, next) {
+  Todo.find(function(err, todo) {
+    if (err) {
+      return next(err);
+    }
+    res.json(todo);
+  });
+})
+.post(function(req, res, next) {
+  var todo = new Todo(req.body);
+  todo.save(function(err, todo) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(todo);
+  });
+});
+
+// Routes that affect a specific ID
+router.route('/todos/:todoId')
+.get(function(req, res, next) {
+  res.json(req.todo);
+})
+// curl -X PUT --data 'title=SomeOtherTitle&order=5&isDone=true&points=2' http://localhost:3000/api/todos/547a5379c4b26e0078c022cd
+.put(function(req, res, next) {
+  var todo = req.todo;
+
+  todo.update(req.body, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.json(todo);
+  });
+})
+.delete(function(req, res, next) {
+  var todo = req.todo;
+  todo.remove();
+  // What's the most appropriate message to return?
+  res.json({message: 'success'});
+});
+
+module.exports = router;
