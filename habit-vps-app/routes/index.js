d2d5cb523dbda49fc524a7977ce1c039ed66b682
Deena Wang <thedeenawang@gmail.com>
2014-11-30 17:31:34 -0500

    Adds put command

--- habit-vps-app/routes/index.js
+++ habit-vps-app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var Todo = mongoose.model('Todo');
 
@@ -23,24 +24,18 @@ router.param('todoId', function(req, res, next, id) {
   });
 });
 
-router.get('/rest/todos', function(req, res, next) {
-  console.log('get request');
-  Todo.find(function(err, todos) {
+// For all routes that don't need an ID
+router.route('/api/todos')
+.get(function(req, res, next) {
+  Todo.find(function(err, todo) {
     if (err) {
       return next(err);
     }
-
-    res.json(todos);
+    res.json(todo);
   });
-});
-
-router.get('/rest/todos/:todoId', function(req, res) {
-  res.json(req.todo);
-});
-
-router.post('/rest/todos', function(req, res, next) {
+})
+.post(function(req, res, next) {
   var todo = new Todo(req.body);
-  console.log(req.body);
   todo.save(function(err, todo) {
     if (err) {
       return next(err);
@@ -49,5 +44,29 @@ router.post('/rest/todos', function(req, res, next) {
     res.json(todo);
   });
 });
+
+// Routes that affect a specific ID
+router.route('/api/todos/:todoId')
+.get(function(req, res, next) {
+  res.json(req.todo);
+})
+// curl -X PUT --data 'title=SomeOtherTitle&order=5&isDone=true&points=2' http://localhost:3000/api/todos/547a5379c4b26e0078c022cd
+.put(function(req, res, next) {
+  var todo = req.todo;
+
+  todo.update(req.body, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.json(todo);
+  });
+})
+.delete(function(req, res, next) {
+
+});
+
+
+
+// This has to be separate. TODO: refactor so that this only returns the active user's todos
 
 module.exports = router;
