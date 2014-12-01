05cc7ab66e30ba7e0d5b6838dac80df56ddbb2bd
Deena Wang <thedeenawang@gmail.com>
2014-12-01 11:49:08 -0500

    Cleanup urls

--- habit-vps-app/public/javascripts/main.js
+++ habit-vps-app/public/javascripts/main.js
@@ -1,9 +1,9 @@
 // This file contains requirejs configuration and the main starting point of the app
 require.config({
   paths: {
-    jquery: '../../bower_components/jquery/dist/jquery.min',
-    underscore: '../../bower_components/underscore-amd/underscore-min',
-    backbone: '../../bower_components/backbone-amd/backbone-min'
+    jquery: '/bower_components/jquery/dist/jquery.min',
+    underscore: '/bower_components/underscore-amd/underscore-min',
+    backbone: '/bower_components/backbone-amd/backbone-min'
   }
 });
 
