cfde8839d9a0cab2822f26ae486a78a266e0d5e9
Deena Wang <thedeenawang@gmail.com>
2014-12-01 11:45:52 -0500

    Sets up require.js

---
+++ habit-vps-app/public/javascripts/main.js
@@ -0,0 +1,13 @@
+// This file contains requirejs configuration and the main starting point of the app
+require.config({
+  paths: {
+    jquery: '../../bower_components/jquery/dist/jquery.min',
+    underscore: '../../bower_components/underscore-amd/underscore-min',
+    backbone: '../../bower_components/backbone-amd/backbone-min'
+  }
+});
+
+define(function(require) {
+  var $ = require('jquery');
+  return {};
+});
